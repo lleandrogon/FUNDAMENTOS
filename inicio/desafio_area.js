44d977702f29c80fc1fd9dc300a24cc6270e4720
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-05-01 17:29:44 -0300

    Fiz mais desafios de JavaScript

---
+++ inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+let raio = 10;
+let areaDaCircunferencia = PI * raio * raio;
+
+console.log("A área da circunferência é de aproximadamente " + areaDaCircunferencia + " metros quadrados.");
+
+areaDaCircunferencia = Math.PI * raio * raio;
+console.log("O valor da área é " + areaDaCircunferencia + " m2.");
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+let temp = a;
+
+console.log("Variáveis antes da troca: a = " + a + " b = " + b);
+
+a = b;
+b = temp;
+
+console.log("Variáveis depois da troca: a = " + a + " b = " + b);
